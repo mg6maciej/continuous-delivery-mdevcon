2841d6b9f4aa878255423a4f4129898584f4e3dc
Maciej Górski <maciek.gorski@gmail.com>
2016-03-17 12:28:57 +0100

    Make 100% test coverage.

--- app/src/androidTest/java/com/elpassion/mdevconworkshopapp/ApplicationTest.java
+++ app/src/androidTest/java/com/elpassion/mdevconworkshopapp/ApplicationTest.java
@@ -1,13 +1,22 @@
 package com.elpassion.mdevconworkshopapp;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
+    }
+
+    public void testHelloWorld() {
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
     }
 }
